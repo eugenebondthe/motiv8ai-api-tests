97fe3372a821b68de79788149c3362e7c0ac4a24
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-25 17:08:07 +0100

    final changes

--- utils/data/fakeDataGenerator.ts
+++ utils/data/fakeDataGenerator.ts
@@ -2,14 +2,14 @@ import { faker } from '@faker-js/faker'
 import { Post, UpdatePost } from '../types/api/posts'
 
 export const getRandomPost = (): Post => ({
-  id: faker.number.int({ min: 1, max: 1000 }),
+  id: faker.number.int({ min: 1, max: 100 }),
   title: faker.lorem.sentence(),
   body: faker.lorem.paragraph(),
-  userId: faker.number.int({ min: 1, max: 1000000 }),
+  userId: faker.number.int({ min: 1, max: 1000 }),
 })
 
 export const getRandomUpdatePost = (): UpdatePost => ({
   title: faker.lorem.sentence(),
   body: faker.lorem.paragraph(),
-  userId: faker.number.int({ min: 1, max: 1000000 }),
+  userId: faker.number.int({ min: 1, max: 10000 }),
 })
